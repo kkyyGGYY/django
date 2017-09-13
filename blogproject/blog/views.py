dff8d6ebc3f0f38c1f195d656b63087a533d2f37
nanfengpo <lsxxxxxx@126.com>
2017-09-13 11:19:48 +0800

    20170913

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -17,7 +17,7 @@ from comments.forms import CommentForm
 class IndexView(ListView):
     model = Post
     template_name = 'blog/index.html'
-    context_object_name = 'post_list'  # 这个name不能瞎取,必须和模板中的变量一样
+    context_object_name = 'post_list'  # 这个post_list不能瞎取,必须和模板中的变量一样
 
 
 # def detail(request, pk):
@@ -39,68 +39,68 @@ class IndexView(ListView):
 #     }
 #     return render(request, 'blog/detail.html',context=context)
 
-# class PostDetailView(DetailView):
-#     model = Post
-#     template_name = 'blog/detail.html'
-#     context_object_name = 'post'
-#
-#     def get(self, request, *args,**kwargs):  # 每次请求就会直接调用get方法,所以点击量的写在get方法中比较合适
-#         # 复写基类的方法,一定要使用super来调用
-#         response = super().get(request, *args,**kwargs)
-#         self.object.increase_views()  # objects 实际上是Post的对象
-#         return response  # 复写基类的方法,该返回的必须要返回
-#
-#     def get_object(self, queryset=None):
-#         post = super().get_object(queryset)
-#         post.body = markdown.markdown(post.body, extensions=[
-#             'markdown.extensions.extra',
-#             'markdown.extensions.codehilite',
-#             'markdown.extensions.toc'
-#         ])
-#         return post
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         form = CommentForm()
-#         # 下面的self.get_object
-#         # 因此self.get_object()必须返回post
-#         comment_list = self.object.comment_set.all()
-#         context.update({  # context实际上已有post,所以这里使用更新update
-#             'form': form,
-#             'comment_list': comment_list,
-#         })
-#         return context
-
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/detail.html'
     context_object_name = 'post'
 
-    def get(self, request, *args, **kwargs):
-        response = super().get(request, *args, **kwargs)
-        self.object.increase_views()  # self.object实际上是Post的对象
-        return response
+    def get(self, request, *args,**kwargs):  # 每次请求就会直接调用get方法,所以点击量的写在get方法中比较合适
+        # 复写基类的方法,一定要使用super来调用
+        response = super().get(request, *args,**kwargs)
+        self.object.increase_views()  # objects 实际上是Post的对象
+        return response  # 复写基类的方法,该返回的必须要返回
 
     def get_object(self, queryset=None):
         post = super().get_object(queryset)
         post.body = markdown.markdown(post.body, extensions=[
             'markdown.extensions.extra',
             'markdown.extensions.codehilite',
-            'markdown.extensions.toc',
+            'markdown.extensions.toc'
         ])
         return post
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)  # 把post加到context
+        context = super().get_context_data(**kwargs)
         form = CommentForm()
-        # 下面的self.object是由self.get_object()得到的
+        # 下面的self.get_object
         # 因此self.get_object()必须返回post
         comment_list = self.object.comment_set.all()
-        context.update({  # context已有post了，所以这里使用更新update
+        context.update({  # context实际上已有post,所以这里使用更新update
             'form': form,
             'comment_list': comment_list,
         })
         return context
+
+# class PostDetailView(DetailView):
+#     model = Post
+#     template_name = 'blog/detail.html'
+#     context_object_name = 'post'
+#
+#     def get(self, request, *args, **kwargs):
+#         response = super().get(request, *args, **kwargs)
+#         self.object.increase_views()  # self.object实际上是Post的对象
+#         return response
+#
+#     def get_object(self, queryset=None):
+#         post = super().get_object(queryset)
+#         post.body = markdown.markdown(post.body, extensions=[
+#             'markdown.extensions.extra',
+#             'markdown.extensions.codehilite',
+#             'markdown.extensions.toc',
+#         ])
+#         return post
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)  # 把post加到context
+#         form = CommentForm()
+#         # 下面的self.object是由self.get_object()得到的
+#         # 因此self.get_object()必须返回post
+#         comment_list = self.object.comment_set.all()
+#         context.update({  # context已有post了，所以这里使用更新update
+#             'form': form,
+#             'comment_list': comment_list,
+#         })
+#         return context
 
 
 # def archives(request, year, month):
